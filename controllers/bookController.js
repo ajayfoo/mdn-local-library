0620255b58fd888d85e5110dcee8af88417a8863
Ajay K <113184147+ajayfoo@users.noreply.github.com>
2024-07-15 23:11:40 +0530

    feat: add layout.ejs

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,7 +1,33 @@
 import asyncHandler from "express-async-handler";
+import Book from "../models/book.js";
+import Author from "../models/author.js";
+import Genre from "../models/genre.js";
+import BookInstance from "../models/bookInstance.js";
 
 const index = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Site Home Page");
+  // Get details of books, book instances, authors and genre counts (in parallel)
+  const [
+    numBooks,
+    numBookInstances,
+    numAvailableBookInstances,
+    numAuthors,
+    numGenres,
+  ] = await Promise.all([
+    Book.countDocuments({}).exec(),
+    BookInstance.countDocuments({}).exec(),
+    BookInstance.countDocuments({ status: "Available" }).exec(),
+    Author.countDocuments({}).exec(),
+    Genre.countDocuments({}).exec(),
+  ]);
+
+  res.render("index", {
+    title: "Local Library Home",
+    book_count: numBooks,
+    book_instance_count: numBookInstances,
+    book_instance_available_count: numAvailableBookInstances,
+    author_count: numAuthors,
+    genre_count: numGenres,
+  });
 });
 
 const book_list = asyncHandler(async (req, res, next) => {

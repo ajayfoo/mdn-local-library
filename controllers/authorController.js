6e0dcd6393781f87d65f8240248414e5ba254d61
Ajay K <113184147+ajayfoo@users.noreply.github.com>
2024-07-15 21:45:23 +0530

    feat: add controllers skeleton

---
+++ controllers/authorController.js
@@ -0,0 +1,44 @@
+import asyncHandler from "express-async-handler";
+
+const author_list = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author list");
+});
+
+const author_detail = asyncHandler(async (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`);
+});
+
+const author_create_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author create GET");
+});
+
+const author_create_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author create POST");
+});
+
+const author_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author delete GET");
+});
+
+const author_delete_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author delete POST");
+});
+
+const author_update_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author update GET");
+});
+
+const author_update_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author update POST");
+});
+
+export {
+  author_list,
+  author_detail,
+  author_create_get,
+  author_create_post,
+  author_delete_get,
+  author_delete_post,
+  author_update_get,
+  author_update_post,
+};
